bd6f662a99d7e5fb24b8716d745a677b846b4d8b
Jordan <jordanclist@gmail.com>
2015-06-10 16:49:00 +1200

    Fix namespace

--- src/MYOB.php
+++ src/MYOB.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Wheniwork\OAuth2\Client\Provider;
+namespace Tenfef\OAuth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
